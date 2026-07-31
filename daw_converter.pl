4177658917475eaea2eb8c14852e20382af15053
danny1marshall1587-maker <danny1marshall1587@gmail.com>
2026-07-31 06:04:01 +0100

    Add/Update daw_converter.pl

--- daw_converter.pl
+++ daw_converter.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
-# REAPER (.rpp) <-> Logic Pro (FCPXML) Converter Engine in Perl
-# 2-Step Selection Support: Explicit Audio Folder + RPP File -> Logic Pro Output
+# REAPER (.rpp) <-> Logic Pro (.logicx / FCPXML) Converter Engine in Perl
+# Native Logic Pro Package Bundle Builder & Audio Media Importer
 
 use strict;
 use warnings;
@@ -14,30 +14,30 @@ my $arg2 = $ARGV[1];
 my $arg3 = $ARGV[2];
 
 if (!$arg1) {
-    print "Usage: daw_converter.pl <rpp_file_or_folder> [<audio_folder>] [<output_folder>]\n";
+    print "Usage: daw_converter.pl <rpp_file_or_folder> [<audio_folder>] [<output_bundle.logicx>]\n";
     exit 1;
 }
 
 my $rpp_file = "";
 my $audio_dir = "";
-my $out_dir = "";
+my $out_bundle = "";
 
 if (-f $arg1) {
     $rpp_file = abs_path($arg1);
     if ($arg2 && -d $arg2) {
         $audio_dir = abs_path($arg2);
-        $out_dir = $arg3 || "";
+        $out_bundle = $arg3 || "";
     } else {
         $audio_dir = dirname($rpp_file);
-        $out_dir = $arg2 || "";
+        $out_bundle = $arg2 || "";
     }
 } elsif (-d $arg1) {
     $audio_dir = abs_path($arg1);
     if ($arg2 && -f $arg2) {
         $rpp_file = abs_path($arg2);
-        $out_dir = $arg3 || "";
+        $out_bundle = $arg3 || "";
     } else {
-        # Find .rpp file in audio directory
+        # Search for .rpp file in directory
         opendir(my $dh, $audio_dir) or die "Cannot open directory $audio_dir: $!";
         my @rpp_files = grep { /\.rpp$/i } readdir($dh);
         closedir($dh);
@@ -47,24 +47,35 @@ if (-f $arg1) {
             exit 1;
         }
         $rpp_file = "$audio_dir/$rpp_files[0]";
-        $out_dir = $arg2 || "";
+        $out_bundle = $arg2 || "";
     }
 }
 
 my ($filename, $dirs, $suffix) = fileparse($rpp_file, qr/\.[^.]*/);
-$out_dir ||= "$audio_dir/${filename}_LogicPro";
+$out_bundle ||= "$audio_dir/${filename}.logicx";
+$out_bundle .= ".logicx" unless $out_bundle =~ /\.logicx$/i;
 
-package_rpp_to_logic_folder($rpp_file, $audio_dir, $out_dir, $filename);
+package_rpp_to_logicx_bundle($rpp_file, $audio_dir, $out_bundle, $filename);
 
-sub package_rpp_to_logic_folder {
+sub package_rpp_to_logicx_bundle {
     my ($in_rpp, $proj_dir, $out_path, $proj_name) = @_;
 
+    # 1. Create Logic Pro bundle structure matching native .logicx format
     my $media_dir = "$out_path/Media/Audio Files";
-    make_path($media_dir);
+    my $res_dir = "$out_path/Resources";
+    my $alts_dir = "$out_path/Alternatives/000";
 
+    make_path($media_dir);
+    make_path($res_dir);
+    make_path($alts_dir);
+
+    # 2. Parse REAPER project
     my $session = parse_rpp($in_rpp);
 
+    # 3. Copy all audio media into Media/Audio Files/
     my $copied_count = 0;
+    my @copied_files = ();
+
     for my $t (@{$session->{tracks}}) {
         for my $i (@{$t->{items}}) {
             if ($i->{file}) {
@@ -91,43 +102,75 @@ sub package_rpp_to_logic_folder {
                     my $dest = "$media_dir/$fname";
                     copy($found, $dest);
                     $i->{abs_file} = abs_path($dest);
+                    push @copied_files, "Media/Audio Files/$fname";
                     $copied_count++;
                 } else {
-                    $i->{abs_file} = "$media_dir/$fname";
+                    $i->{abs_file} = abs_path("$media_dir/$fname");
                 }
             }
         }
     }
 
-    # Generate FCPXML inside output directory with valid file:/// URIs
+    # Also scan audio directory for any additional audio files (.wav, .aif, .mp3)
+    if (-d $proj_dir) {
+        opendir(my $adh, $proj_dir);
+        while (my $af = readdir($adh)) {
+            next if $af =~ /^\./;
+            if ($af =~ /\.(wav|aif|aiff|mp3|m4a|flac)$/i) {
+                my $src_file = "$proj_dir/$af";
+                my $dst_file = "$media_dir/$af";
+                if (! -f $dst_file) {
+                    copy($src_file, $dst_file);
+                    push @copied_files, "Media/Audio Files/$af";
+                    $copied_count++;
+                }
+            }
+        }
+        closedir($adh);
+    }
+
+    # 4. Write ProjectInformation.plist inside Resources/
+    my $info_plist = "$res_dir/ProjectInformation.plist";
+    open my $ip, '>:encoding(UTF-8)', $info_plist;
+    print $ip "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    print $ip "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n";
+    print $ip "<plist version=\"1.0\">\n<dict>\n";
+    print $ip "  <key>BundleVersion</key>\n  <string>12</string>\n";
+    print $ip "  <key>HasProjectFolder</key>\n  <true/>\n";
+    print $ip "  <key>LastSavedFrom</key>\n  <string>Logic Pro</string>\n";
+    print $ip "  <key>PROJECT_NAME</key>\n  <string>$proj_name</string>\n";
+    print $ip "</dict>\n</plist>\n";
+    close $ip;
+
+    # 5. Generate FCPXML inside bundle with valid file:/// URIs
     my $fcpxml_path = "$out_path/Session.fcpxml";
     open my $out, '>:encoding(UTF-8)', $fcpxml_path or die "Could not write $fcpxml_path: $!";
     print $out get_fcpxml_str($session, $out_path, $proj_name);
     close $out;
 
-    # Open launcher script
+    # 6. Generate Open in Logic Pro launcher script
     my $launcher_path = "$out_path/Open in Logic Pro.command";
     open my $lout, '>:encoding(UTF-8)', $launcher_path;
     print $lout "#!/bin/bash\nDIR=\"\$( cd \"\$( dirname \"\${BASH_SOURCE[0]}\" )\" >/dev/null 2>&1 && pwd )\"\nopen -a \"Logic Pro\" \"\$DIR/Session.fcpxml\"\n";
     close $lout;
     chmod 0755, $launcher_path;
 
-    # Instructions text file
+    # 7. Instructions text file
     my $readme_path = "$out_path/How to Open in Logic Pro.txt";
     open my $rout, '>:encoding(UTF-8)', $readme_path;
-    print $rout "REAPER to Logic Pro Converted Project Folder\n";
-    print $rout "=============================================\n\n";
+    print $rout "Logic Pro Package Bundle (.logicx)\n";
+    print $rout "===================================\n\n";
     print $rout "Project Name: $proj_name\n";
-    print $rout "Audio Folder Source: $proj_dir\n";
-    print $rout "Tracks Count: " . scalar(@{$session->{tracks}}) . "\n\n";
-    print $rout "To open this project in Logic Pro:\n";
-    print $rout " 1. Double-click 'Open in Logic Pro.command' in this folder.\n";
-    print $rout " 2. Or open Logic Pro and choose File > Import > Final Cut Pro XML...\n";
-    print $rout "    and select 'Session.fcpxml' in this folder.\n\n";
-    print $rout "All audio files are bundled inside 'Media/Audio Files/'.\n";
+    print $rout "Tracks Count: " . scalar(@{$session->{tracks}}) . "\n";
+    print $rout "Audio Media Files Bundled: $copied_count\n\n";
+    print $rout "To open in Logic Pro:\n";
+    print $rout " 1. Double-click 'Open in Logic Pro.command' inside this bundle.\n";
+    print $rout " 2. Or open Logic Pro, go to File > Import > Final Cut Pro XML...\n";
+    print $rout "    and choose 'Session.fcpxml' inside this bundle.\n\n";
+    print $rout "All audio media is stored natively inside 'Media/Audio Files/'.\n";
     close $rout;
 
-    print "🎉 Successfully created Logic Pro project folder: '$out_path'\n";
+    print "🎉 Successfully created native Logic Pro bundle: '$out_path'\n";
     print "   Tracks: " . scalar(@{$session->{tracks}}) . " | Audio Files Bundled: $copied_count\n";
 }
 
@@ -204,7 +247,7 @@ sub get_fcpxml_str {
 
     for my $t (@{$session->{tracks}}) {
         for my $i (@{$t->{items}}) {
-            my $abs_path = $i->{abs_file} || "$base_dir/$i->{name}.wav";
+            my $abs_path = $i->{abs_file} || "$base_dir/Media/Audio Files/$i->{name}.wav";
             $abs_path =~ s#\\#/#g;
             $abs_path = "/$abs_path" unless $abs_path =~ m#^/#;
             
@@ -235,7 +278,7 @@ sub get_fcpxml_str {
         $role = "track_$t->{num}" if !$role;
 
         for my $i (@{$t->{items}}) {
-            my $abs_path = $i->{abs_file} || "$base_dir/$i->{name}.wav";
+            my $abs_path = $i->{abs_file} || "$base_dir/Media/Audio Files/$i->{name}.wav";
             $abs_path =~ s#\\#/#g;
             $abs_path = "/$abs_path" unless $abs_path =~ m#^/#;
             
